d07bb73dfce4d0b251f2696edd5595bdfb2a3f91
Joshua Newberry <joshuamnewberry@gmail.com>
2024-10-04 15:23:25 -0400

    Add files via upload

---
+++ Halfling Chess/creatures.py
@@ -0,0 +1,115 @@
+from character import *
+
+class Villain(Character):
+    def __init__(self) -> None:
+        super().__init__(Player.VILLAIN)
+    
+    def is_valid_move(self, from_coord:Coord, to_coord:Coord, board:List[List[None|Character]]) -> bool:
+        if from_coord.x == to_coord.x and from_coord.y == to_coord.y:
+            return False
+        
+    
+    def is_valid_attack(self, from_coord: Coord, to_coord: Coord, board:List[List[None|Character]]) -> bool:
+        if from_coord.x == to_coord.x and from_coord.y == to_coord.y:
+            return False
+        
+    
+    def calculate_dice(self, target:Character, attack:bool = True, lst:list = [], *args, **kwargs) -> int:
+        pass
+
+    def deal_damage(self, target:Character, damage:int, *args, **kwargs) -> None:
+        pass
+
+class Goblin(Villain):
+    def __init__(self) -> None:
+        super().__init__()
+        self.health = 3
+        self.temp_health = 3
+        self.combat = [2, 2]
+
+class Skeleton(Villain):
+    def __init__(self) -> None:
+        super().__init__()
+        self.health = 2
+        self.temp_health = 2
+        self.combat = [2, 1]
+        self.move = 2
+
+class Necromancer(Villain):
+    def __init__(self) -> None:
+        super().__init__()
+        self.combat = [1, 2]
+        self.range = 3
+    
+    def raise_dead(target:Character) -> None:
+        target.player = Player.VILLAIN
+        target.temp_health = int(target.health / 2)
+
+class Hero(Character):
+    def __init__(self) -> None:
+        super().__init__(Player.HERO)
+    
+    def is_valid_move(self, from_coord:Coord, to_coord:Coord, board:List[List[None|Character]]) -> bool:
+        if from_coord.x == to_coord.x and from_coord.y == to_coord.y:
+            return False
+        
+    
+    def is_valid_attack(self, from_coord:Coord, to_coord:Coord, board:List[List[None|Character]]) -> bool:
+        if from_coord.x == to_coord.x and from_coord.y == to_coord.y:
+            return False
+        
+    
+    def calculate_dice(self, target:Character, attack:bool = True, lst:list = [], *args, **kwargs) -> int:
+        pass
+
+    def deal_damage(self, target:Character, damage:int, *args, **kwargs) -> None:
+        pass
+
+class Warrior(Hero):
+    def __init__(self) -> None:
+        super().__init__()
+        self.health = 7
+        self.temp_health = 7
+        self.combat = [2, 4]
+    
+    def calculate_dice(self, target:Character, attack:bool = True, lst:list = [], gob:list = []) -> None:
+        pass
+
+class Mage(Hero):
+    def __init__(self) -> None:
+        super().__init__()
+        self.combat = [2, 2]
+        self.range = 3
+        self.move = 2
+    
+    def deal_damage(self, target:Character, damage:int):
+        pass
+
+class Paladin(Hero):
+    def __init__(self) -> None:
+        super().__init__()
+        self.__heal = True
+        self.health = 6
+        self.temp_health = 6
+    
+    @property
+    def heal(self) -> bool:
+        return self.__heal
+    @heal.setter
+    def heal(self, heal) -> None:
+        if type(heal) != bool:
+            raise TypeError
+        self.__heal = heal
+    
+    def revive(self, target:Character) -> None:
+        if self.__heal:
+            target.temp_health = int(target.health / 2)
+            self.__heal = False
+
+class Ranger(Hero):
+    def __init__(self) -> None:
+        super().__init__()
+        self.range = 3
+    
+    def deal_damage(self, target:Character, damage:int):
+        pass
---
+++ Halfling Chess/game.py
@@ -0,0 +1,206 @@
+from __future__ import annotations
+import os
+from typing import List, Optional
+from dungeon import *
+from creatures import *
+from coord import *
+
+class Game:
+    def __init__(self) -> None:
+        self.dungeon = Dungeon(8, 8, [])
+        self.rooms_cleared = 0
+        self.moves = 0
+        self.atk = 0
+        self.selected = None
+    
+    def setup(self) -> None:
+        self.dungeon.place_heroes()
+        self.dungeon.place_villains()
+    
+    def play(self) -> None:
+        while not (self.dungeon.is_dungeon_clear() and not self.dungeon.adventurer_defeat()):
+            clear()
+            self.print_display()
+            self.select()
+            while self.atk <= 1 and self.moves < 2:
+                x = self.action()
+                print(x)
+                if x is None:
+                    clear()
+                    self.print_display()
+                    continue
+                elif x:
+                    print('Next Turn')
+                    break
+                else:
+                    print('Try again!')
+                clear()
+                self.print_display()
+            self.selected = None
+            self.end_turn()
+    
+    def attack(self) -> None:
+        l = input('Please enter the coordinate you wish to attack: ')
+        loc = [int(i) for i in l.split()]
+        to = Coord(loc[0], loc[1])
+        s = self.find_character(self.selected)
+        start = Coord(s[0], s[1])
+        self.dungeon.attack(start, to)
+        self.atk += 1
+    
+    def move(self) -> None:
+        print('in move')
+        co = self.find_character(self.selected)
+        mv = [Coord(co[0], co[1])]
+        q = ''
+        if type(self.selected) == Hero:
+
+            while len(mv) < self.selected.move or q != 'q':
+                try:
+                    q = input('Select a coordinate (x, y): ')
+
+                    if q == 'q':
+                        break
+                    l = [int(i) for i in q.split()]
+                    print(l)
+                    if 0 <= l[0] < self.dungeon.height and 0 <= l[0] < self.dungeon.width:
+                        mv.append(Coord(l[0], l[1]))
+                    else:
+                        raise ValueError
+                except ValueError:
+                    print('Invalid coordinate, try again.')
+                    continue
+                except:
+                    print('Something went wrong, try again')
+            if self.dungeon.is_valid_move(mv):
+                self.dungeon.move(mv[0], mv[-1])
+                self.moves += 1
+                return
+        else:
+            while len(mv) <= 1:
+                try:
+                    q = input('Select a coordinate (x, y): ')
+                    if q == 'q':
+                        break
+                    l = [int(i) for i in q.split()]
+                    print(l)
+                    if 0 <= l[0] < self.dungeon.height and 0 <= l[0] < self.dungeon.width:
+                        mv.append(Coord(l[0], l[1]))
+                    else:
+                        raise ValueError
+                except ValueError:
+                    print('Invalid coordinate, try again.')
+                    continue
+                except:
+                    print('Something went wrong, try again')
+                    continue
+            if self.dungeon.is_valid_move(mv):
+                self.dungeon.move(mv[0], mv[-1])
+                self.moves += 1
+    
+    def raise_dead(self) -> None:
+        co = self.find_character(self.selected)
+        coords = [Coord(co[0], co[1])]
+        loc = input('Please enter the coordinates you wish to attack: ')
+        end = [int(i) for i in loc.split()]
+        coords.append(Coord(end[0], end[1]))
+        if isinstance(self.dungeon.board[coords[0].x][coords[0].y], Necromancer):
+            if self.dungeon.is_valid_attack(coords):
+                self.dungeon.board[coords[0].x][coords[0].y].raise_dead(
+                    self.board[coords[1].x][coords[1].y])
+                self.attack += 1
+    
+    def heal(self) -> None:
+        co = self.find_character(self.selected)
+        coords = [Coord(co[0], co[1])]
+        loc = input('Please enter the coordinates you wish to attack: ')
+        end = [int(i) for i in loc.split()]
+        coords.append(Coord(end[0], end[1]))
+        if isinstance(self.dungeon.board[coords[0].x][coords[0].y], Paladin):
+            if self.dungeon.is_valid_attack(coords):
+                self.dungeon.board[coords[0].x][coords[0].y].heal(
+                    self.board[coords[1].x][coords[1].y])
+                self.attack += 1
+    
+    def find_character(self, char: Character):
+        for i in range(len(self.dungeon.board)):
+            if char in self.dungeon.board[i]:
+                return (i, self.dungeon.board[i].index(char))
+    
+    def select(self) -> None:
+        coords = input(
+            f'Please select two numbers 0-{self.dungeon.height-1} and 0-{self.dungeon.width-1} to select your piece: ')
+        coords = coords.split()
+        x, y = [int(i) for i in coords]
+        self.selected = self.dungeon.board[x][y]
+        while self.selected == None:
+            coords = input(
+                f'Please select two numbers 0-{self.dungeon.height-1} and 0-{self.dungeon.width-1} to select your piece: ')
+            coords = coords.split()
+            x, y = [int(i) for i in coords]
+            self.selected = self.dungeon.board[x][y]
+        print(f'{self.selected.__class__.__name__} has been selected')
+    
+    def action(self):
+        print('Please select one of the following actions:')
+        lst = ['attack', 'move']
+        print('\t1. Move\n\t2. Attack', end='')
+        if isinstance(self.selected, Necromancer):
+            print('\n\t3. Raise Dead\n\t4. End Turn')
+            lst.append('raise dead')
+        elif isinstance(self.selected, Paladin):
+            print('\n\t3. Heal\n\t4. End Turn')
+            lst.append('heal')
+        else:
+            print('\n\t3. End Turn')
+        lst.append('end turn')
+        selection = input()
+        return self.choices(selection, lst)
+    
+    def end_turn(self) -> None:
+        self.moves = 0
+        self.attack = 0
+        self.dungeon.set_next_player()
+    
+    def choices(self, st, lst) -> bool|None:
+        if st.lower() in lst:
+            if st == 'move' and self.moves <= 1:
+                self.move()
+                return None
+            elif st.lower() == 'attack' and self.atk < 1:
+                self.attack()
+                return None
+            elif st.lower() == 'end turn':
+                return True
+            elif st.lower() == 'heal' and self.atk < 1:
+                self.heal()
+                return None
+            elif st.lower() == 'raise dead' and self.atk < 1:
+                self.raise_dead()
+                return None
+            else:
+                return False
+    
+    def print_display(self) -> None:
+        print('=====================Villains======================')
+
+        for v in self.dungeon.villains:
+            print(
+                f'{v.__class__.__name__:<12}{v.temp_health}/{v.health} HP\tLOC -> {self.find_character(v)}')
+        print('---------------------------------------------------')
+        self.dungeon.print_board()
+        print('===================Heroes==========================')
+        for h in self.dungeon.heroes:
+            print(
+                f'{h.__class__.__name__:<12}{h.temp_health}/{h.health} HP\tLOC -> {self.find_character(h)}')
+        print('---------------------------------------------------')
+        print(f'{self.dungeon.player.name}\'s turn')
+
+def clear() -> None:
+    command = ('cls' if os.name == 'nt' else 'clear')
+    os.system(command)
+
+if __name__ == "__main__":
+    g = Game()
+    g.setup()
+    g.play()
