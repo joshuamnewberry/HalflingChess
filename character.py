dd11153e8021371686fd05057ac6a208a5a9b137
Joshua Newberry <joshuamnewberry@gmail.com>
2024-10-07 21:06:54 -0400

    Update character.py

--- character.py
+++ character.py
@@ -6,8 +6,12 @@ from random import randint
 from coord import Coord
 
 class CharacterDeath(Exception):
-    def __init__(self, msg:str, char:Character):
+    def __init__(self, msg:str, char:Character) -> None:
         self.message = msg
+        self.char = char
+    
+    def __str__(self) -> str:
+        return ""
 
 class Player(Enum):
     VILLAIN = 0
@@ -25,6 +29,7 @@ class Character(ABC):
         self.__move = 3
         self.__range = 1
     
+    @abstractmethod
     def __str__(self) -> str:
         return self.__class__.__name__
     
@@ -46,7 +51,7 @@ class Character(ABC):
         return self.__health
     @health.setter
     def health(self, health:int) -> None:
-        self.integerType(health)
+        Character.integerType(health)
         if health <= 0:
             raise ValueError
         self.__health = health
@@ -56,16 +61,18 @@ class Character(ABC):
         return self.__temp_health
     @player.setter
     def temp_health(self, temp_health:int) -> None:
-        self.integerType(temp_health)
+        Character.integerType(temp_health)
         self.__temp_health = temp_health
+        if self.__temp_health < 0:
+            raise CharacterDeath
     
     @property
     def combat(self) -> list:
         return [self.__attack, self.__defense]
     @combat.setter
-    def attack(self, combat:list) -> None:
-        self.integerType(combat[0])
-        self.integerType(combat[1])
+    def combat(self, combat:list) -> None:
+        Character.integerType(combat[0])
+        Character.integerType(combat[1])
         if combat[0] < 0 or combat[1] < 0:
             raise ValueError
         self.__attack = combat[0]
@@ -95,16 +102,53 @@ class Character(ABC):
     def is_valid_move(self, from_coord:Coord, to_coord:Coord, board:List[List[None|Character]]) -> bool:
         if from_coord.x == to_coord.x and from_coord.y == to_coord.y:
             return False
-        
+        height = len(board)
+        width = len(board[0])
+        if from_coord.x < 0 or to_coord.x < 0 or from_coord.y < 0 or to_coord.y < 0:
+            return False
+        if from_coord.x > width or to_coord.x > width or from_coord.y > height or to_coord.y > height:
+            return False
+        if board[from_coord.x][from_coord.y] != self:
+            return False
+        if board[to_coord.x][to_coord.y] != None:
+            return False
+        return True
+    
     @abstractmethod
     def is_valid_attack(self, from_coord:Coord, to_coord:Coord, board:List[List[None|Character]]) -> bool:
         if from_coord.x == to_coord.x and from_coord.y == to_coord.y:
             return False
+        height = len(board)
+        width = len(board[0])
+        if from_coord.x < 0 or to_coord.x < 0 or from_coord.y < 0 or to_coord.y < 0:
+            return False
+        if from_coord.x > width or to_coord.x > width or from_coord.y > height or to_coord.y > height:
+            return False
+        if board[from_coord.x][from_coord.y] != self:
+            return False
+        if not isinstance(board[to_coord.x][to_coord.y], Character):
+            return False
+        return True
     
     @abstractmethod
     def calculate_dice(self, target:Character, attack:bool = True, lst:list = [], *args, **kwargs) -> int:
-        pass
+        num = 0
+        compare = 3
+        if attack:
+            compare = 4
+        if lst == []:
+            for i in range(1, attack):
+                if randint(1, 6) > compare:
+                    num += 1
+        else:
+            for i in lst:
+                if i > compare:
+                    num += 1
+        return num
 
     @abstractmethod
     def deal_damage(self, target:Character, damage:int, *args, **kwargs) -> None:
-        pass
+        try:
+            target.__temp_health -= damage
+        except CharacterDeath:
+            print()
